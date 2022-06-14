96fa20f4a4d3aa654ea8167de97f57055b62c95d
Mayer Olivares <mayerolivares_15@hotmail.com>
2022-06-14 19:50:16 -0400

    Se agrega la api rest customer

---
+++ customer-service/src/main/java/com/olivares/store/customer/repository/CustomerRepository.java
@@ -0,0 +1,15 @@
+package com.olivares.store.customer.repository;
+
+import com.olivares.store.customer.repository.entity.Customer;
+import com.olivares.store.customer.repository.entity.Region;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    public Customer findByNumberID(String numberID);
+
+    public List<Customer> findByLastName(String lastName);
+
+    public List<Customer> findByRegion(Region region);
+}
---
+++ customer-service/src/main/java/com/olivares/store/customer/service/CustomerService.java
@@ -0,0 +1,23 @@
+package com.olivares.store.customer.service;
+
+import com.olivares.store.customer.repository.entity.Customer;
+import com.olivares.store.customer.repository.entity.Region;
+
+import java.util.List;
+
+public interface CustomerService {
+
+    public List<Customer> findCustomerAll();
+
+    public List<Customer> findCustomersByRegion(Region region);
+
+    public Customer createCustomer(Customer customer);
+
+    public Customer updateCustomer(Customer customer);
+
+    public Customer deleteCustomer(Customer customer);
+
+    public Customer getCustomer(Long id);
+
+
+}
