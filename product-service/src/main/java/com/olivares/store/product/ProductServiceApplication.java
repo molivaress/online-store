14cfa1279174b9e24a922e75f9cc194f02d7c1c6
molivaress <mayerolivares_15@hotmail.com>
2022-06-15 17:11:32 -0400

    - enable eureka client in product-service, shopping-service

--- product-service/src/main/java/com/olivares/store/product/ProductServiceApplication.java
+++ product-service/src/main/java/com/olivares/store/product/ProductServiceApplication.java
@@ -2,12 +2,14 @@ package com.olivares.store.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ProductServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServiceApplication.class, args);
+    }
 
 }
--- shopping-service/src/main/java/com/olivares/store/shopping/ShoppingServiceApplication.java
+++ shopping-service/src/main/java/com/olivares/store/shopping/ShoppingServiceApplication.java
@@ -2,12 +2,14 @@ package com.olivares.store.shopping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ShoppingServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShoppingServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingServiceApplication.class, args);
+    }
 
 }
