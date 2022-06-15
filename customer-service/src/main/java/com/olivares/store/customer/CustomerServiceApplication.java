9cc300726d7a8786b5a2df9b578fdae820298589
molivaress <mayerolivares_15@hotmail.com>
2022-06-15 16:00:52 -0400

    enable eureka client in customer service

--- customer-service/src/main/java/com/olivares/store/customer/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/olivares/store/customer/CustomerServiceApplication.java
@@ -2,12 +2,14 @@ package com.olivares.store.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class CustomerServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CustomerServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
 
 }
