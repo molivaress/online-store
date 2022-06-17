ff01d15794778195aabced03b08c30ee894ab01a
molivaress <mayerolivares_15@hotmail.com>
2022-06-16 21:43:26 -0400

    - add hixtrix in shopping service

--- shopping-service/src/main/java/com/olivares/store/shopping/ShoppingServiceApplication.java
+++ shopping-service/src/main/java/com/olivares/store/shopping/ShoppingServiceApplication.java
@@ -3,10 +3,14 @@ package com.olivares.store.shopping;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @EnableEurekaClient
+@EnableHystrix
+@EnableHystrixDashboard
 @SpringBootApplication
 public class ShoppingServiceApplication {
 
--- shopping-service/src/main/java/com/olivares/store/shopping/client/model/Customer.java
+++ shopping-service/src/main/java/com/olivares/store/shopping/client/model/Customer.java
@@ -1,8 +1,10 @@
 package com.olivares.store.shopping.client.model;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class Customer {
     private Long id;
     private String numberID;
